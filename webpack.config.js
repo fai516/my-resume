5cc8b8743319e73858042cb0f35d2ae721144067
fai516 <yfsiuv@gmail.com>
2019-06-19 08:57:34 +0800

    add modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: path.join(__dirname, "./dist"),
+    publicPath: "",
+    filename: "./scripts/bundle.js"
   },
-
-  // Enable sourcemaps for debugging webpack's output.
+  plugins: [new HtmlWebpackPlugin()],
   devtool: "source-map",
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
@@ -22,13 +22,8 @@ module.exports = {
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM"
+  // }
 };
